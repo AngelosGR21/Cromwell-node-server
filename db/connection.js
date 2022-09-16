6f256836e6f3ed7b70df5cb6a5ce578d1dc400f1
AngelosGR21 <angelgrigoriu@gmail.com>
2022-09-16 16:57:27 +0100

    updated connection file to access .local

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,13 @@
 const {Pool} = require("pg");
 
+const ENV = process.env.NODE_ENV || "development";
+
 require("dotenv").config({
-    path: `${__dirname}/../.env.development`
+    path: `${__dirname}/../.env.${ENV}`
 })
 
 if(!process.env.PGDATABASE){
     throw new Error('PGDATABASE is not set')
 }
 
-module.exports = new Pool(config)
+module.exports = new Pool()
