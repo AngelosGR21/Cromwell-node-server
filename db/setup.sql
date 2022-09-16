dd36f3ca97855de1b84f95f48b5e7320f02aea4a
AngelosGR21 <angelgrigoriu@gmail.com>
2022-09-16 15:33:25 +0100

    creating databases

---
+++ db/setup.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS cromwell_server_data_test;
+DROP DATABASE IF EXISTS cromwell_server_data;
+
+CREATE DATABASE cromwell_server_data_test;
+CREATE DATABASE cromwell_server_data;
