9914768b65c3ad9a3ffa29ce2ad611389dbbd541
AngelosGR21 <angelgrigoriu@gmail.com>
2022-09-16 16:57:41 +0100

    adding test-data

---
+++ db/data/test-data.js
@@ -0,0 +1,22 @@
+const users = [
+    {
+        firstName: "Paul",
+        lastName: "Statham",
+        email: "paul@gmail.com",
+        password: "",
+    },
+    {
+        firstName: "Alex",
+        lastName: "Mele",
+        email: "alex@gmail.com",
+        password: "",
+    },
+    {
+        firstName: "Lili",
+        lastName: "Orinthia",
+        email: "lili@gmail.com",
+        password: "",
+    }
+]
+
+module.exports = users
