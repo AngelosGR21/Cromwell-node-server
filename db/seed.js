285909d6441ef7cbb509423624c610df755b7698
AngelosGR21 <angelgrigoriu@gmail.com>
2022-09-16 16:56:45 +0100

    created seed for the test db

---
+++ db/seed.js
@@ -0,0 +1,36 @@
+const format = require("pg-format");
+const db = require("./connection")
+const testData = require("./data/test-data")
+
+const createUserTable = async () => {
+
+    await db.query(`DROP TABLE IF EXISTS users;`);
+
+    await db.query(`
+        CREATE TABLE users (
+            first_name VARCHAR NOT NULL,
+            last_name VARCHAR NOT NULL,
+            email VARCHAR NOT NULL,
+            password VARCHAR NOT NULL
+        );
+    `)
+
+}
+
+const seed = async () => {
+    
+    await createUserTable()
+    const formattedUserData = testData.map(({firstName, lastName, email, password}) => [firstName, lastName, email, password])
+
+    const insertUsersData = format(
+        `INSERT INTO users (first_name, last_name, email, password) VALUES %L RETURNING *;`, formattedUserData
+    )
+    
+    return db.query(insertUsersData).then((result) => {
+        console.log(result)
+        console.log(result.rows)
+        db.end()
+    });
+}
+
+seed()
