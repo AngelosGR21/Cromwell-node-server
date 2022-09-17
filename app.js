ec46fc3c070f56bb70a6ce712573cbd89aa7eec5
AngelosGR21 <angelgrigoriu@gmail.com>
2022-09-17 23:20:37 +0100

    added POST route /user/register and moved endRequest in utils directory

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const http = require("http")
 const server = http.createServer()
 
+const {postUser} = require("./controllers")
+const endRequest = require("./utils/endRequest")
+
 server.on("request", async (req, res) => {
     if(req.url === "/" && req.method === "GET"){
         endRequest(res, 200, "Api is up and running")
+    }else if(req.url === "/user/register" && req.method === "POST"){
+        postUser(req, res)
     }
 })
-
-
-function endRequest (res, statusCode, data) {
-    res.statusCode = statusCode
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify({data}))
-}
-
 
 
 module.exports = server
