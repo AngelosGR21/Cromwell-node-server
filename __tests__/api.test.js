6a7695bc23c75a78d663f8e679d1794bdc5a9023
AngelosGR21 <angelgrigoriu@gmail.com>
2022-09-17 23:21:36 +0100

    added tests for the POST route /users/register

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -19,3 +19,87 @@ describe("GET - '/'", () => {
         })
     })
 })
+
+describe("POST - /user/register", () => {
+    test("returns 200", () => {
+        const user = {
+            firstName: "Daniel",
+            lastName: "Williams",
+            email: "daniel@gmail.com",
+            password: "DanielWilliams25@"
+        }
+        
+        return request(server)
+            .post("/user/register")
+            .send(user)
+            .expect(200)
+            .then((res) => {
+                const {data} = res.body;
+                expect(data).toBe("User has been created");
+            })
+    })
+    test("returns 400 when a field is missing", () => {
+        const user = {
+            firstName: "Daniel",
+            lastName: "Williams",
+            email: "daniel@gmail.com",
+        }
+        return request(server)
+            .post("/user/register")
+            .send(user)
+            .expect(400)
+            .then((res) => {
+                const {data} = res.body;
+                expect(data).toBe("Bad request");
+            })
+    })
+    test("returns 400 when email is invalid", () => {
+        const user = {
+            firstName: "Daniel",
+            lastName: "Williams",
+            email: "daniel]@@@gmail.com",
+            password: "DanielWilliams25@"
+        }
+        return request(server)
+            .post("/user/register")
+            .send(user)
+            .expect(400)
+            .then((res) => {
+                const {data} = res.body;
+                expect(data).toBe("Bad request");
+            })
+
+    })
+    test("returns 400 when password is not strong enough", () => {
+        const user = {
+            firstName: "Daniel",
+            lastName: "Williams",
+            email: "daniel@gmail.com",
+            password: "12345"
+        }
+        return request(server)
+            .post("/user/register")
+            .send(user)
+            .expect(400)
+            .then((res) => {
+                const {data} = res.body;
+                expect(data).toBe("Bad request");
+            })
+    })
+    test("returns 409 when an email is already taken", () => {
+        const user = {
+            firstName: "Daniel",
+            lastName: "Williams",
+            email: "lili@gmail.com",
+            password: "DanielWilliams25@"
+        }
+        return request(server)
+            .post("/user/register")
+            .send(user)
+            .expect(409)
+            .then((res) => {
+                const {data} = res.body;
+                expect(data).toBe("Email is already in use");
+            })
+    })
+})
