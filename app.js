37aef272f3164e5252898b38cce38170ccf09f25
AngelosGR21 <angelgrigoriu@gmail.com>
2022-09-18 00:49:38 +0100

    added POST route /user/login

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http")
 const server = http.createServer()
 
-const {postUser} = require("./controllers")
+const {postUser, loginUser} = require("./controllers")
 const endRequest = require("./utils/endRequest")
 
 server.on("request", async (req, res) => {
@@ -9,6 +9,8 @@ server.on("request", async (req, res) => {
         endRequest(res, 200, "Api is up and running")
     }else if(req.url === "/user/register" && req.method === "POST"){
         postUser(req, res)
+    }else if(req.url === "/user/login" && req.method === "POST"){
+        loginUser(req, res)
     }
 })
 
