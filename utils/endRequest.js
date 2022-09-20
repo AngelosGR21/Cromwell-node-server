9e87d4964a057224fae5ac1343be552773dce929
AngelosGR21 <angelgrigoriu@gmail.com>
2022-09-20 16:40:15 +0100

    adding headers to allow requests

--- utils/endRequest.js
+++ utils/endRequest.js
@@ -2,6 +2,8 @@ function endRequest (res, statusCode, data, token) {
     res.statusCode = statusCode
     if(token) res.setHeader("Authorization", `Bearer ${token}`);
     res.setHeader("Content-Type", "application/json")
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET")
     res.end(JSON.stringify({data}))
 }
 
