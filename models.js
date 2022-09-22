fa3a4348df5a4615e233c4af04ca6a8df71f6fe3
AngelosGR21 <angelgrigoriu@gmail.com>
2022-09-22 19:26:34 +0100

    updating queries

--- models.js
+++ models.js
@@ -5,7 +5,7 @@ module.exports.insertUser = ( reqBody ) => {
     const {email, firstName, lastName, hashedPassword} = reqBody
     const userDetails = [firstName, lastName, email, hashedPassword]  
     return db.query(`INSERT INTO users (first_name, last_name, email, password)
-    VALUES ($1, $2, $3, $4) RETURNING id, first_name, last_name, email`, userDetails).then((res) => {
+    VALUES ($1, $2, $3, $4) RETURNING id, first_name as "firstName", last_name AS "lastName", email`, userDetails).then((res) => {
         return res.rows[0]
     })
 }
@@ -13,18 +13,13 @@ module.exports.insertUser = ( reqBody ) => {
 module.exports.verifyUser = ( reqBody ) => {
     const {email, password} = reqBody
 
-    return db.query(`SELECT * FROM users 
+    return db.query(`SELECT password, id, email, first_name AS "firstName", last_name AS "lastName" FROM users 
     WHERE email = $1`, [email]).then( (res) => {
         if(!res.rows[0]) return Promise.reject({statusCode: 401, message: "Invalid email or password", customError: true})
-        
         const user = res.rows[0];
         return bcrypt.compare(password, user.password).then((correct) => {
             if(!correct) return Promise.reject({statusCode: 401, message: "Invalid email or password", customError: true})
             delete user.password;
-            user.firstName = user.first_name;
-            user.lastName = user.last_name;
-            delete user.first_name
-            delete user.last_name
             
             return user;
         })
